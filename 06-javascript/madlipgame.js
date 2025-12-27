a9b7aa0d1297a9b7c2760318cfcdbf62c9207ecf
Mohammedashik1809 <mohammedashik7866@gmail.com>
2025-12-27 22:50:55 +0530

    added madlipgame.html

---
+++ 06-javascript/madlipgame.js
@@ -0,0 +1,7 @@
+let name = 'John';
+let adjective ='Shiny';
+let noun ='unicorn';
+let verb ='dance';
+let place ='kitchen';
+
+alert(`One day, ${name} found a ${adjective} ${noun} that could ${verb} in the ${place}.`)
